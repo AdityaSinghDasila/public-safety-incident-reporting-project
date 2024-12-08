e46d3897448ea55880bb058ba897b96fcd6d8cac
Aditya Singh <adityasinghdasila15@gmail.com>
2024-12-08 16:27:47 +0530

    phone number requires 10 characters

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app = express();
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
 app.get("/",(req,res)=>{
     res.sendFile(__dirname + "/public/index.html");
     console.log("sent the landing page :");
@@ -18,15 +19,22 @@ app.get("/user_register",(req,res)=>{
     console.log("A user was sent the register page ");
 });
 
-app.get("/user_signin",(req,res)=>{
+app.get("/user_signin_page",(req,res)=>{
     res.sendFile(__dirname+"/public/resident-signin.html");
     console.log("A user was sent the resident-signin page");
 })
 
-app.get("/admin_signin",(req,res)=>{
+app.get("/admin_signin_page",(req,res)=>{
     res.sendFile(__dirname+"/public/admin-signin.html");
     console.log("A user was sent the admin-signin page");
 })
+
+
+//user sign in attempt : 
+app.post("/user_signin",(req,res)=>{
+    console.log("phone number recieved : "+ req.body.user_phone);
+    console.log("password recieved : "+ req.body.user_password);
+});
 
 
 app.listen(port,(req,res)=>{
